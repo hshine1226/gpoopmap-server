96cdf1cd5b646ede8afde4f2e005fba73e48babc
hshine1226 <hshine1226@gmail.com>
2020-08-23 15:55:00 +0900

    Add global router

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -2,4 +2,8 @@ import express from "express";
 
 const globalRouter = express.Router();
 
+globalRouter.get("/", function (req, res) {
+  res.json({ message: "대똥여지도 server입니다." });
+});
+
 export default globalRouter;
