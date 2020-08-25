caafa7aa06b2021b7e221aee7c67c5d11f461f81
hshine1226 <hshine1226@gmail.com>
2020-08-25 17:23:05 +0900

    Update cors options for dynamic origin

--- src/app.js
+++ src/app.js
@@ -15,11 +15,23 @@ import MongoStore from "connect-mongo";
 
 import "./passport";
 
+const allowList = ["http://localhost:3000", "https://gpoopmap.netlify.app/"];
+var corsOptionsDelegate = function (req, callback) {
+  console.log(req.header("Origin"));
+  var corsOptions;
+  if (allowList.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true, credentials: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
 const app = express();
 
 const CookieStore = MongoStore(session);
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(helmet()); // Express App에 도움을 주는 미들웨어
 app.use(cookieParser()); // Session을 다루기 위한 미들웨어
 app.use(bodyParser.json()); // 서버가 json을 이해하게 해주는 미들웨어
@@ -40,6 +52,6 @@ app.use(passport.session());
 
 // Routers
 app.use(routes.home, globalRouter);
-app.use(routes.api, apiRouter);
+app.use(routes.api, cors(corsOptionsDelegate), apiRouter);
 
 export default app;
